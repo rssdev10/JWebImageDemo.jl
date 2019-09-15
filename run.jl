5a2c32adb5ff5afb127d3fd7594e92206e5c7064
Roman S Samarev <samarev@acm.org>
2019-09-15 22:42:01 +0300

    added explicit __init__() calls for PackageCompiler

--- run.jl
+++ run.jl
@@ -1,3 +1,13 @@
 #!/usr/bin/env julia --project=@.
 
+# this part is required for PackageCompiler
+import Sockets
+Sockets.__init__()
+
+import HTTP.URIs
+import HTTP.Parsers
+URIs.__init__()
+Parsers.__init__()
+# end of PackageCompiler
+
 include("src/server.jl")
