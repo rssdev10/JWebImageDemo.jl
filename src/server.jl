486e95b6b043c4cc74a876eda3610dc674f8596e
Roman S Samarev <samarev@acm.org>
2020-12-04 10:48:33 +0300

    updated HTTP multipart related code

--- src/server.jl
+++ src/server.jl
@@ -8,7 +8,7 @@ using JWebImageDemo
 
 function get_server()
     if haskey(ENV, "ON_HEROKU")
-        (port=parse(Int, ENV["PORT"]), host=Sockets.IPAddr(0,0,0,0))
+        (port=parse(Int, ENV["PORT"]), host=ip"0.0.0.0")
     else
         (port=8080, host=ip"127.0.0.1")
     end
@@ -22,45 +22,43 @@ end
 
 import HTTP, HTTP.Parsers
 
-"""
-Workaround while this feature is not implemented in HTTP.jl
-https://www.ietf.org/rfc/rfc2616.txt
-"""
-function parse_multipart(data::Vector{UInt8})
-    chunk_end = HTTP.Parsers.find_end_of_chunk_size(data)
-    if chunk_end == 0
-        return nothing
-    end
+get(multipart::Vector{HTTP.Multipart}, name::String) =
+    findfirst(part -> part.name == name, multipart) |>
+    idx -> isnothing(idx) ? nothing : multipart[idx]
 
-    boundary = data[1:chunk_end - 2] # without \r\n
-    header_end = HTTP.Parsers.find_end_of_header(data)
-
-    data_start = header_end + 1
-    data_end = length(data) - chunk_end - 2
-    # check that ending is with same boundary + two additional hyphens
-    if (data[data_end + 1: end] == [boundary..., 0x2d, 0x2d, 0x0d, 0x0a])
-        result = data[data_start : data_end]
-        length(result) > 10 && return result # fails on empty new lines
-    end
-    return nothing
-end
+# function get(multipart::Vector{HTTP.Multipart}, name::String)::Union{HTTP.Multipart, Nothing}
+#     for part in multipart
+#         if part.name == name
+#             return part
+#         end
+#     end
+#     return nothing
+# end
 
 """
 Process incomming query with image file
 Generate output image in PNG format
 """
 function process_image(c::WelcomeController)
-    data = parse_multipart(c.conn.request.body)
-    # dump(c.conn.request)
-    if data != nothing
-        buf = IOBuffer(data)
-        img = FileIO.load(Stream(format"PNG", buf))
-        out_img = JWebImageDemo.permute_image(img) # , 5)
-        out_buf = IOBuffer()
-        FileIO.save(Stream(format"PNG", out_buf), out_img)
-        return Render("image/png", take!(out_buf))
-    end
-    render(Text, "Please upload some image file")
+    local multiparts = HTTP.parse_multipart_form(c.conn.request)
+
+    # dump(multiparts)
+    local mp_file = get(multiparts, "f_image")
+    local mp_num = get(multiparts, "num")
+
+    isnothing(mp_file) &&
+        return render(Text, "Please upload some image file in f_image field")
+    isnothing(mp_num) &&
+        return render(Text, "Please specify number of sections in num field")
+
+    local img = FileIO.load(Stream(format"PNG", mp_file.data))
+    local out_img = JWebImageDemo.permute_image(
+        img,
+        tryparse(Int, String(take!(mp_num.data))) |> x -> something(x, 5),
+    )
+    local out_buf = IOBuffer()
+    FileIO.save(Stream(format"PNG", out_buf), out_img)
+    return Render(mp_file.contenttype, identity, take!(out_buf))
 end
 
 routes() do
