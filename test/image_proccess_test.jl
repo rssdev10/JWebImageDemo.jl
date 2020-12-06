91cc0df1d284d14f95741373fd43d7cc098dabf4
Roman S Samarev <samarev@acm.org>
2020-12-06 18:18:16 +0300

    extended image permite test

--- test/image_proccess_test.jl
+++ test/image_proccess_test.jl
@@ -1,12 +1,20 @@
+using JWebImageDemo
+using FileIO
+using Test
 
 const TEST_IMAGE = joinpath(@__DIR__, "resources/green_python.jpg")
 const NUM = 5
 
-@testset "JWebImageDemo.jl" begin
+const TMP_PATH = "tmp"
+isdir(TMP_PATH) || mkdir(TMP_PATH)
+
+@testset "image permute test" begin
     img = FileIO.load(TEST_IMAGE)
-    @time img_out = JWebImageDemo.permute_image(img, NUM)
-    #save(Stream(format"PNG",io), data...)
-    @test size(img) != size(img_out) ||
-          any(((i, o),) -> i != o, zip(iterate(img), iterate(img_out)))
-    save("out.jpg", img_out)
+    for i = 2:NUM
+        @time img_out = JWebImageDemo.permute_image(img, i)
+        #save(Stream(format"PNG",io), data...)
+        @test size(img) != size(img_out) ||
+            any(((i, o),) -> i != o, zip(iterate(img), iterate(img_out)))
+        save(joinpath(TMP_PATH, "out_$i.jpg"), img_out)
+    end
 end
